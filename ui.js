53291504b01025fc03d0b236e9cf753e75c41de2
Gfg207 <superszymon21@gmail.com>
2026-04-21 14:56:32 +0200

    Remove VRAM Monitor and Language Matrix code
    Removed VRAM Monitor and Language Matrix functionality.

--- ui.js
+++ ui.js
@@ -1,14 +1,1 @@
-// VRAM Monitor
-let vram = 75;
-setInterval(() => {
-  vram = Math.max(10, Math.min(99, vram + (Math.random() * 10 - 5)));
-  document.getElementById("vramValue").textContent = vram.toFixed(0) + "%";
-}, 1000);
-
-// Language Matrix
-const languages = ["PL", "EN", "DE", "ES", "FR"];
-let langIndex = 0;
-setInterval(() => {
-  langIndex = (langIndex + 1) % languages.length;
-  document.getElementById("lang").textContent = languages[langIndex];
-}, 3000);
+//this time nothing
